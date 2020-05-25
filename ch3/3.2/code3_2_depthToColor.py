f7c10b8f1c386d5e7f6311efc87b10378bbdc5a3
WWH_SJTU <wangweihang_china@163.com>
2020-05-25 14:50:21 +0800

    Open-Source code for 3D World version0525

--- ch3/3.2/code3_2_depthToColor.py
+++ ch3/3.2/code3_2_depthToColor.py
@@ -5,8 +5,18 @@
 #   输出参数：
 #   伪彩色图
 import cv2
-# 读取深度图
-img_dep = cv2.imread("box.png", -1)
+import numpy as np
 
+# 读取深度图并8bit可视化
+img_src = cv2.imread("src.png", -1)
+img_dep = (img_src / 16).astype(np.uint8)
+cv2.imshow("img_dep", img_dep)
+
+# 图像归一化处理
+img_dep = (img_dep - np.min(img_dep)) / (np.max(img_dep) - np.min(img_dep)) * 255
+img_dep = img_dep.astype(np.uint8)
 # 伪彩色图转换
-img_rgb = cv2.applyColorMap(img_dep, cv2.COLORMAP_RAINBOW)
+img_rgb = cv2.applyColorMap(img_dep, cv2.COLORMAP_JET)
+
+cv2.imshow("img_rgb", img_rgb)
+cv2.waitKey(0)
